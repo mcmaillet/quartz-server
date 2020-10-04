2ba1ce056b3479e250b8ba3ba92ac0ed0776b977
Mark Maillet <mark.maillet@hedhome.com>
2020-10-04 14:30:19 -0600

    add swagger

---
+++ src/main/java/com/scheduler/scheduler/HomeController.java
@@ -0,0 +1,12 @@
+package com.scheduler.scheduler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(path = "/")
+    public String index() {
+        return "redirect:/swagger-ui.html";
+    }
+}
